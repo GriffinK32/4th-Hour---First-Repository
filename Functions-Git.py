1fcdef0fe2fff2c3587022e8ff75ae331408341c
Korbin Griffin <02korbin.griffin@gulllakecs.org>
2019-02-11 13:00:46 -0500

    Add the Functions and Variables

--- Functions-Git.py
+++ Functions-Git.py
@@ -1,7 +1,7 @@
 # 4.13.3: Greetings
 # Korbin Griffin
 # 2.5.19
-
+'''
 name = input ("What is your name: ")
 
 def greeting():
@@ -9,3 +9,17 @@ def greeting():
     print("Nice to meet you")
 
 greeting()
+
+'''
+# 4.13.4: Functions and Variables
+# Korbin Griffin
+# 2.11.19
+
+x =  406
+
+def print_something():
+    x = 3
+    print('\r\n', x)
+
+print('\r\n', x)
+print_something()
