d05f542d255d3fc20eecbe1ec49ba3ce5c8759bd
Korbin Griffin <02korbin.griffin@gulllakecs.org>
2019-02-27 12:56:07 -0500

    Add Functions and Variables, Part 3

--- Functions-Git.py
+++ Functions-Git.py
@@ -1,7 +1,7 @@
 # 4.13.3: Greetings
 # Korbin Griffin
 # 2.5.19
-'''
+
 name = input ("What is your name: ")
 
 def greeting():
@@ -10,12 +10,12 @@ def greeting():
 
 greeting()
 
-'''
+
 # 4.13.4: Functions and Variables
 # Korbin Griffin
 # 2.11.19
 
-x =  406
+x =  10
 
 def print_something():
     x = 3
@@ -23,3 +23,15 @@ def print_something():
 
 print('\r\n', x)
 print_something()
+
+
+# 4.13.6: Functions and Variables, Part 3
+# Korbin Griffin
+# 2.18.19
+
+
+def print_number(x):
+    print('\n' + x)
+
+print_number(13)
+print_number(23)
